4550312ccaddffc4bf9f93f7544d34611f7af108
Walid <walid.bensalah20@gmail.com>
2025-08-29 19:34:48 +0200

    ✨ F01: LOGIN > Add Title and form

---
+++ src/components/pages/LoginPage.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function LoginPage() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrez votre prénom"
+          value={inputValue}
+          onChange={handleChange}
+          required
+        />
+        <button>Accédez à votre espace</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
