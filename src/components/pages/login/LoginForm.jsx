02cc232fad538b73d81ef8f6ed175f1e8b410a1d
Walid <walid.bensalah20@gmail.com>
2025-08-31 18:02:48 +0200

    ✨ F02: LOGIN > add redirection from/to OrderPage

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function LoginForm() {
   const [inputValue, setInputValue] = useState("");
@@ -25,7 +26,9 @@ export default function LoginForm() {
         onChange={handleChange}
         required
       />
-      <button>Accédez à votre espace</button>
+      <Link to="/order">
+        <button>Accédez à votre espace</button>
+      </Link>
     </form>
   );
 }
