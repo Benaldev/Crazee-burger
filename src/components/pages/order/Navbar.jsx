8bed6ec11f70b662cec33ce94f49d10c054ec3dd
Walid <walid.bensalah20@gmail.com>
2025-09-15 18:36:32 +0200

    ♻️ F04: ORDER > Navbar > Create NavbarRightSide

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -1,16 +1,11 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
+import NavbarRightSide from "./NavbarRightSide";
 
 export default function Navbar({ username }) {
   return (
     <NavbarStyled>
       <div className="left-side">left</div>
-      <div className="right-side">
-        <h1>Bonjour {username}</h1>
-        <Link to="/">
-          <button>Déconnexion</button>
-        </Link>
-      </div>
+      <NavbarRightSide username={username} />
     </NavbarStyled>
   );
 }
@@ -22,8 +17,5 @@ const NavbarStyled = styled.nav`
 
   .left-side {
     background: purple;
-  }
-  .right-side {
-    background: pink;
   }
 `;
