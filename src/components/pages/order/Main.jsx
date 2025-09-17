bcdf5e2a027e0dba76d45442a811cf775b824d3c
Walid <walid.bensalah20@gmail.com>
2025-09-17 19:26:22 +0200

    💄 F04: ORDER > Add styling

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
+import { theme } from "../../../theme";
 
 export default function Main() {
-  return <MainStyled>Main</MainStyled>;
+  return <MainStyled></MainStyled>;
 }
 
 const MainStyled = styled.div`
   flex: 1;
-  background-color: green;
+  background-color: ${theme.colors.background_white};
+  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 `;
