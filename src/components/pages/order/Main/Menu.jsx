ad32ac276edf35f51c0fcbe4a16be50c5709ef57
Walid <walid.bensalah20@gmail.com>
2025-10-04 22:06:21 +0200

    🚧 F05: ORDER > MENU > Add grid layout

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
 
 export default function Menu() {
-  return <MenuStyled className="menu">Menu</MenuStyled>;
+  const [menu, setMenu] = useState(fakeMenu2);
+
+  return (
+    <MenuStyled className="menu">
+      {menu.map((produit) => (
+        <div className="produit" key={produit.id}>
+          {produit.title}
+        </div>
+      ))}
+    </MenuStyled>
+  );
 }
 
 const MenuStyled = styled.div`
   border: 1px solid red;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-row-gap: 60px;
+  padding: 50px 50px 150px;
+  justify-items: center;
+
+  .produit {
+    background: green;
+    width: 240px;
+    height: 330px;
+  }
 `;
