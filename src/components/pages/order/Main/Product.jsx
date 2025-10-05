84ca7c384e49a9026e5cbac84079880d074d7da2
Walid <walid.bensalah20@gmail.com>
2025-10-05 15:48:29 +0200

    💄 F05: ORDER > Product > Add image layout

--- src/components/pages/order/Main/Product.jsx
+++ src/components/pages/order/Main/Product.jsx
@@ -7,8 +7,8 @@ export default function Product({ title, imageSource, price, id }) {
         <img src={imageSource} alt={title} />
       </div>
       <div className="info-text">
+        <div className="title">{title}</div>
         <div className="description">
-          <div className="title">{title}</div>
           <div className="price">{price}</div>
           <button className="add-button">Ajouter</button>
         </div>
@@ -19,22 +19,27 @@ export default function Product({ title, imageSource, price, id }) {
 
 const ProductSyled = styled.div`
   background: green;
-  width: 240px;
-  height: 330px;
+  width: 200px;
+  height: 300px;
   display: grid;
+  grid-template-rows: 65% 1fr;
+  padding: 20px;
+  padding-bottom: 10px;
 
   .image {
-    width: 100px;
+    width: 100%;
     height: auto;
-    border: 1px solid fuchsia;
+    border: 1px solid yellow;
+    margin-top: 30px;
 
     img {
       width: 100%;
       height: 100%;
-      object-fit: cover;
+      object-fit: contain;
     }
   }
 
-  .description {
+  .info-text {
+    border: 1px solid blue;
   }
 `;
