43f82ed3f063a6c72c33c3ea0ec8bfd71d8f2df6
Walid <walid.bensalah20@gmail.com>
2025-10-09 00:20:02 +0200

    ✨ F06: NAVBAR > Add notification toast on admin button click

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,15 +1,38 @@
 import styled from "styled-components";
 import Profile from "./Profile";
 import ToggleButton from "../../../reusable-ui/ToggleButton";
+import { ToastContainer, toast } from "react-toastify";
+import { useState } from "react";
 
 export default function NavbarRightSide({ username }) {
+  const [isModeAdmin, setIsModeAdmin] = useState(false);
+
+  const displayToastNotification = () => {
+    if (!isModeAdmin) {
+      toast.info("Mode admin activé", {
+        // icon: <FaUserSecret size={30} />,
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    setIsModeAdmin(!isModeAdmin);
+  };
+
   return (
     <NavbarRightSideStyle>
       <ToggleButton
         labelIfUnchecked="Activer le mode admin"
         labelIfChecked="Désactiver le mode admin"
+        onToggle={displayToastNotification}
       />
       <Profile username={username} />
+      <ToastContainer className="toaster" bodyClassName="body-toast" />
     </NavbarRightSideStyle>
   );
 }
