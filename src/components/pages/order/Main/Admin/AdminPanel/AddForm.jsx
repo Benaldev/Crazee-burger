bb0d1a0641bcbdcc65470c99c70a222f57ce41ce
Walid <walid.bensalah20@gmail.com>
2025-11-16 04:28:12 +0100

    ♻️F09: ADMIN > Add a reusable Form

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -1,12 +1,9 @@
 import { useContext, useState } from "react";
-import styled from "styled-components";
 import OrderContext from "../../../../../../context/OrderContext.jsx";
-import TextInput from "../../../../../reusable-ui/TextInput.jsx";
 import Button from "../../../../../reusable-ui/Button.jsx";
-import ImagePreview from "./ImagePreview.jsx";
 import SubmitMessage from "./SubmitMessage.jsx";
-import { getInputTextsConfig } from "./inputTextConfig.jsx";
 import { EMPTY_PRODUCT } from "../../../../../../enums/product.js";
+import Form from "./Form.jsx";
 
 export default function AddForm() {
   //state
@@ -39,62 +36,16 @@ export default function AddForm() {
     }, 2000);
   };
 
-  const inputTexts = getInputTextsConfig(newProduct);
-
-  //affichage
   return (
-    <AddFormStyled onSubmit={handleSubmit}>
-      <ImagePreview
-        imageSource={newProduct.imageSource}
-        title={newProduct.title}
-      />
-      <div className="input-fields">
-        {inputTexts.map((input) => (
-          <TextInput
-            {...input}
-            key={input.id}
-            onChange={handleChange}
-            version="minimalist"
-          />
-        ))}
-      </div>
-      <div className="submit">
+    <Form product={newProduct} onSubmit={handleSubmit} onChange={handleChange}>
+      <>
         <Button
           className="submit-button"
           label={"Ajouter un nouveau produit au menu"}
-          version={"success"}
+          version="success"
         />
         {isSubmitted && <SubmitMessage />}
-      </div>
-    </AddFormStyled>
+      </>
+    </Form>
   );
 }
-
-const AddFormStyled = styled.form`
-  display: grid;
-  grid-template-columns: 1fr 3fr;
-  grid-template-rows: repeat(4, 1fr);
-  height: 100%;
-  width: 70%;
-  grid-row-gap: 8px;
-  grid-column-gap: 20px;
-
-  .input-fields {
-    grid-area: 1 / 2 / 4 / 3;
-
-    display: grid;
-    grid-row-gap: 8px;
-  }
-
-  .submit {
-    grid-area: 4 / 2 / 5 / 3;
-    display: flex;
-    align-items: center;
-    position: relative;
-    top: 3px;
-
-    .submit-button {
-      height: 100%;
-    }
-  }
-`;
--- src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
@@ -1,15 +1,11 @@
 import { useContext } from "react";
 import OrderContext from "../../../../../../context/OrderContext";
-import styled from "styled-components";
-import { getInputTextsConfig } from "./inputTextConfig";
-import ImagePreview from "./ImagePreview";
-import TextInput from "../../../../../reusable-ui/TextInput";
 import EditInfoMessage from "./EditInfoMessage";
+import Form from "./Form";
 
 export default function EditForm() {
   const { productSelected, setProductSelected, handleEdit, titleEditRef } =
     useContext(OrderContext);
-  const inputTexts = getInputTextsConfig(productSelected);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -21,54 +17,8 @@ export default function EditForm() {
   };
 
   return (
-    <EditFormStyled>
-      <ImagePreview
-        imageSource={productSelected.imageSource}
-        title={productSelected.title}
-      />
-      <div className="input-fields">
-        {inputTexts.map((input) => (
-          <TextInput
-            {...input}
-            key={input.id}
-            onChange={handleChange}
-            version="minimalist"
-            ref={input.name === "title" ? titleEditRef : null}
-          />
-        ))}
-      </div>
-      <div className="submit">
-        <EditInfoMessage />
-      </div>
-    </EditFormStyled>
+    <Form product={productSelected} onChange={handleChange} ref={titleEditRef}>
+      <EditInfoMessage />
+    </Form>
   );
 }
-
-const EditFormStyled = styled.form`
-  display: grid;
-  grid-template-columns: 1fr 3fr;
-  grid-template-rows: repeat(4, 1fr);
-  height: 100%;
-  width: 70%;
-  grid-row-gap: 8px;
-  grid-column-gap: 20px;
-
-  .input-fields {
-    grid-area: 1 / 2 / 4 / 3;
-
-    display: grid;
-    grid-row-gap: 8px;
-  }
-
-  .submit {
-    grid-area: 4 / 2 / 5 / 3;
-    display: flex;
-    align-items: center;
-    position: relative;
-    top: 3px;
-
-    .submit-button {
-      height: 100%;
-    }
-  }
-`;
