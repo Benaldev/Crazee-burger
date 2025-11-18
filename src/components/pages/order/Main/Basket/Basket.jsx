668beeb916611f63a3ceb267d2ab9bdc85eab8ec
Walid <walid.bensalah20@gmail.com>
2025-11-18 03:05:51 +0100

    ♻️ F10: BASKET > Add Total

--- src/components/pages/order/Main/Basket/Basket.jsx
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
 import Header from "../../../../reusable-ui/Header";
+import Total from "./Total";
+import { formatPrice } from "../../../../../utils/maths";
 
 export default function Basket() {
   return (
     <BasketStyles>
-      <Header>Head</Header>
+      <Total amountToPay={formatPrice()} />
       <div className="body">Body</div>
       <Header>Footer</Header>
     </BasketStyles>
