00b45e2069b3c4068325c6ebde86c167f4cb1da0
Walid <walid.bensalah20@gmail.com>
2025-11-18 05:25:36 +0100

    ♻️ F11: ORDER > Add useMenu

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -4,50 +4,17 @@ import Main from "./Main/Main";
 import { theme } from "../../../theme";
 import { useRef, useState } from "react";
 import OrderContext from "../../../context/OrderContext";
-import { fakeMenu } from "../../../fakeData/fakeMenu";
 import { EMPTY_PRODUCT } from "../../../enums/product";
-import { deepClone } from "../../../utils/array";
+import { useMenu } from "../../../hooks/useMenu";
 
 export default function OrderPage() {
   const [isModeAdmin, setIsModeAdmin] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [currentTabSelected, setCurrentTabSelected] = useState("add");
-  const [menu, setMenu] = useState(fakeMenu.MEDIUM);
   const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
   const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
   const titleEditRef = useRef();
-
-  const handleAdd = (newProduct) => {
-    const menuCopy = deepClone(menu);
-    const menuUpdated = [newProduct, ...menuCopy];
-    setMenu(menuUpdated);
-  };
-
-  const handleDelete = (idOfProductToDelelte) => {
-    const menuCopy = deepClone(menu);
-
-    const menuUpdated = menuCopy.filter(
-      (product) => product.id !== idOfProductToDelelte
-    );
-
-    setMenu(menuUpdated);
-  };
-
-  const handleEdit = (productBeigEdited) => {
-    const menuCopy = deepClone(menu);
-
-    const indexOfProductToEdit = menu.findIndex(
-      (product) => product.id === productBeigEdited.id
-    );
-
-    menuCopy[indexOfProductToEdit] = productBeigEdited;
-
-    setMenu(menuCopy);
-  };
-
-  const resetMenu = () => {
-    setMenu(fakeMenu.MEDIUM);
-  };
+  const { menu, handleAdd, handleDelete, handleEdit, resetMenu } = useMenu();
 
   const orderContextValue = {
     isModeAdmin,
