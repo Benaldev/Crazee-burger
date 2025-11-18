5dc11e8a74f2eef93c171f4f20b4228916b13db4
Walid <walid.bensalah20@gmail.com>
2025-11-18 03:19:40 +0100

    ♻️ F10: BASKET > Add Body

--- src/components/pages/order/Main/Basket/Basket.jsx
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
-import { theme } from "../../../../../theme";
 import Total from "./Total";
 import { formatPrice } from "../../../../../utils/maths";
 import Footer from "./Footer";
+import BasketBody from "./BasketBody";
 
 export default function Basket() {
   return (
     <BasketStyles>
       <Total amountToPay={formatPrice()} />
-      <div className="body">Body</div>
+      <BasketBody />
       <Footer />
     </BasketStyles>
   );
@@ -18,10 +18,4 @@ const BasketStyles = styled.div`
   background-color: pink;
   display: flex;
   flex-direction: column;
-
-  .body {
-    flex: 1;
-    background: ${theme.colors.background_white};
-    box-shadow: ${theme.shadows.basket};
-  }
 `;
