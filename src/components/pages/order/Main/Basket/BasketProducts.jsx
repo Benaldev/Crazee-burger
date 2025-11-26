949a555ba20a02dd741ca48c910381ecdc815166
Walid <walid.bensalah20@gmail.com>
2025-11-26 22:07:47 +0100

    💄 F11: BASKET > Add BasketCard (Read only from fakeBasket.LARGE)

--- src/components/pages/order/Main/Basket/BasketProducts.jsx
+++ src/components/pages/order/Main/Basket/BasketProducts.jsx
@@ -1,3 +1,29 @@
-export default function BasketProducts() {
-  return <div>BasketProducts</div>;
+import styled from "styled-components";
+import BasketCard from "./BasketCard";
+
+export default function BasketProducts({ basket }) {
+  return (
+    <BasketProductsStyled>
+      {basket.map((basketProduct) => (
+        <div className="basket-card" key={basketProduct.id}>
+          <BasketCard {...basketProduct} />
+        </div>
+      ))}
+    </BasketProductsStyled>
+  );
 }
+
+const BasketProductsStyled = styled.div`
+  /* border: 1px solid red; */
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+
+  .basket-card {
+    /* border: 1px solid blue; */
+    margin: 10px 16px;
+    height: 86px;
+    box-sizing: border-box;
+  }
+`;
