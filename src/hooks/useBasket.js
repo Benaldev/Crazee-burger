4136d9e0814e7250842bf16ed53af37ce098028d
Walid <walid.bensalah20@gmail.com>
2025-12-01 21:14:22 +0100

    ✨ F11: BASKET > Add Product > Create handleAddToBasket (when product is ALREADY in basket) with increment

--- src/hooks/useBasket.js
+++ src/hooks/useBasket.js
@@ -3,7 +3,7 @@ import { fakeBasket } from "../fakeData/fakeBasket";
 import { deepClone, find } from "../utils/array";
 
 export const useBasket = () => {
-  const [basket, setBasket] = useState(fakeBasket.MEDIUM);
+  const [basket, setBasket] = useState(fakeBasket.SMALL);
 
   const handleAddToBasket = (productToAdd) => {
     //1. copie du state
@@ -11,7 +11,7 @@ export const useBasket = () => {
 
     const isProductAlreadyInBasket =
       find(productToAdd.id, basketCopy) !== undefined;
-    console.log("isProductAlreadyInBasket: ", isProductAlreadyInBasket);
+    // console.log("isProductAlreadyInBasket: ", isProductAlreadyInBasket)
 
     //2. manip de la copie du state
     // 1er cas : le produit n'est pas déjà dans le basket
@@ -25,9 +25,21 @@ export const useBasket = () => {
 
       //3. update du state
       setBasket(basketUpdated);
-    }
+    } else {
+      // 2ème cas : le produit n'est déjà dans le basket
+      const indexOfBasketProductToIncrement = basket.findIndex(
+        (basketProduct) => basketProduct.id === productToAdd.id
+      );
 
-    // 2ème cas : le produit est déjà dans le basket
+      // console.log("indexOfBasketProductToIncrement: ", indexOfBasketProductToIncrement)
+      console.log(
+        "basket[indexOfBasketProductToIncrement]: ",
+        basketCopy[indexOfBasketProductToIncrement]
+      );
+      basketCopy[indexOfBasketProductToIncrement].quantity += 1;
+      //3. update du state
+      setBasket(basketCopy);
+    }
   };
 
   return { basket, handleAddToBasket };
