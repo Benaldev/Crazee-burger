315d6b22dddc17ec1a0aae30ca64c3b27cd30aba
Walid <walid.bensalah20@gmail.com>
2026-01-18 11:34:39 +0100

    ✨ F14: BASKET > Total > Add CasinoEffect (finished)

--- src/components/reusable-ui/CasinoEffect.jsx
+++ src/components/reusable-ui/CasinoEffect.jsx
@@ -1,3 +1,30 @@
+import styled from "styled-components";
+
 export default function CasinoEffect({ count, className }) {
-  return <span className={className}>{count}</span>;
+  return (
+    <CasinoEffectStyled>
+      <span key={count} className={className}>
+        {count}
+      </span>
+    </CasinoEffectStyled>
+  );
 }
+
+const CasinoEffectStyled = styled.div`
+  position: relative;
+  overflow: hidden;
+
+  span {
+    display: inline-block;
+    animation: slideIn 300ms ease forwards;
+  }
+
+  @keyframes slideIn {
+    from {
+      transform: translateY(100%);
+    }
+    to {
+      transform: translateY(0%);
+    }
+  }
+`;
